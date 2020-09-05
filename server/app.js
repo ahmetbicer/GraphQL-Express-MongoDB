448d9a7f003068d2f4e8d35fd2fe484a07ea1dd5
ahmetbicer <ahmtbicertr@gmail.com>
2020-09-05 20:19:05 +0300

    basic setup for graphql added

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
 
 const app = express()
+
+app.use("/graphql", graphqlHTTP({
+    
+}))
 
 app.listen(3000, () => console.log("server started"))
